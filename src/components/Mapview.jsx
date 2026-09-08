c1dff61349459c707d32f6896a4e89e018579f99
alananogueeira <cerqueiraalana20@gmail.com>
2026-09-08 18:00:01 -0300

    Update Mapview.jsx

--- src/components/Mapview.jsx
+++ src/components/Mapview.jsx
@@ -190,26 +190,24 @@ function MapView() {
         return (
             <div className="gps-screen">
                 <div className="gps-card">
-                {erroLocalizacao ? (
-                        <>
-                            <h2>Não foi possível acessar sua localização</h2>
-                    
-                            <button className="gps-retry-btn" onClick={solicitarLocalizacao}>
-                                Tentar novamente
-                            </button>
-                        </>
-                    ) : (
-                        <>
-                            <div className="loader"></div>
-                            <h2>Encontrando sua localização</h2>
-                            <p>
-                                Para exibir o mapa corretamente, precisamos acessar sua localização.
-                            </p>
-                            <span className="gps-subtext">
-                                Aguarde enquanto buscamos sua posição...
-                            </span>
-                        </>
-                    )}
+                    <div className="loader"></div>
+
+                    <h2>Encontrando sua localização</h2>
+
+                    <p>
+                        Para exibir o mapa corretamente, precisamos acessar sua localização.
+                    </p>
+
+                    <span className="gps-subtext">
+                        Permita o acesso à localização no navegador.
+                    </span>
+
+                    <button
+                        className="gps-retry-btn"
+                        onClick={solicitarLocalizacao}
+                    >
+                        Tentar novamente
+                    </button>
                 </div>
             </div>
         );
